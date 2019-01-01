342aed193c87e6d17b611cda8b758fc3d61ffba7
yutoj <yutoogata4@gmail.com>
2019-01-01 16:12:43 +0900

    sample component

---
+++ src/App/Weclome.js
@@ -0,0 +1,5 @@
+import React, { Component}  from 'react';
+
+export default function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Weclome';
 
 class App extends Component {
   render() {
     return (
-      <div> Hello World </div>
+      <Welcome name={"guest"}/>
     );
   }
 }
