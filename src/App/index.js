6ae03c98b83e307b5c371c59d0e5426d70e12aba
yutoj <yutoogata4@gmail.com>
2019-01-01 16:31:34 +0900

    styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import Welcome from './Weclome';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+  ${props => props.primary && css`
+    background: white;
+    color: palevioletred;
+  `}
+`
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+`
 
 class App extends Component {
   render() {
     return (
-      <Welcome name={"guest"}/>
+      <div>
+        <Welcome name={"guest"} />
+        <MyButton>hello </MyButton>
+        <MyButton primary>hello </MyButton>
+        <TomatoButton primary>hello </TomatoButton>
+      </div>
     );
   }
 }
