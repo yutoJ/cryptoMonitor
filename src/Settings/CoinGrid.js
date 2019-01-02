288331767c06e6e86c9fea3a4085d9a6561c3426
yutoj <yutoogata4@gmail.com>
2019-01-02 15:16:32 +0900

    delete icon

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -10,16 +10,16 @@ grid-template-columns: repeat(5, 1fr);
 grid-gap: 10px;
 margin-top: 40px;
 `
-function getCoinsToDisplay(coinList) {
-    return Object.keys(coinList).slice(0,100); 
+function getCoinsToDisplay(coinList, topSection) {
+    return Object.keys(coinList).slice(0, topSection ? 10 : 100); 
 }
 
-export default function () {
+export default function ({topSection}) {
     return <AppContext.Consumer>
         {({coinList}) => 
         <CoinGridStyled>
-            {getCoinsToDisplay(coinList).map(coinKey =>
-                <CoinTile coinKey={coinKey} />
+            {getCoinsToDisplay(coinList, topSection).map(coinKey =>
+                <CoinTile topSection={topSection} coinKey={coinKey} />
             )}
         </CoinGridStyled>
         }
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -8,6 +8,7 @@ export default function() {
     return <div> <Page name="settings">
                 <Welcome />
                 <ConfirmButton />
+                <CoinGrid topSection />
                 <CoinGrid />
             </Page>
         </div>
