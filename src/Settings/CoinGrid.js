1363eb2b7cd26c74462d62f8db133a3bba99727f
yutoj <yutoogata4@gmail.com>
2019-01-02 14:12:32 +0900

    coin tile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../App/AppProvider';
+import {Tile} from "../Shared/Tile";
+import { greenBackgroundColor, greenBoxShadow } from '../Shared/Styles';
 
 export const CoinGridStyled = styled.div`
 display: grid;
 grid-template-columns: repeat(5, 1fr);
+grid-gap: 10px;
 `
+
+export const SelectableTile = styled(Tile)`
+    &:hover {
+        cursor: pointer;
+        ${greenBoxShadow}
+    }`
 
 export default function () {
     return <AppContext.Consumer>
         {({coinList}) => 
         <CoinGridStyled>
             {Object.keys(coinList).map(coinKey =>
-            <div> {coinKey}</div>)}
+            <SelectableTile> {coinKey}</SelectableTile>)}
         </CoinGridStyled>
         }
     </AppContext.Consumer>
---
+++ src/Shared/Tile.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import {subtleBoxShadow, lightBoxShadow, greenBoxShadow, redBoxShadow, lightBlueBackground} from '../Shared/Styles';
+
+export const Tile = styled.div`
+    ${subtleBoxShadow}
+    ${lightBlueBackground}
+    padding: 10px;
+`
